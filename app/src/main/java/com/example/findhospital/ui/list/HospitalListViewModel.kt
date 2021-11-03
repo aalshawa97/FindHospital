c44d0dc33a7b8cc047f74d97828630bdd353975a
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-03 13:54:11 -0700

    Databinding

--- app/src/main/java/com/example/findhospital/ui/list/HospitalListViewModel.kt
+++ app/src/main/java/com/example/findhospital/ui/list/HospitalListViewModel.kt
@@ -5,23 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HospitalListViewModel : ViewModel() {
-    /*
-   private val name : String = "";
-   private val latitude : double = 0.0;
-   private val longitude : double = 0.0;
 
+   private var name : String = "";
+   private var latitude : Double = 0.0;
+   private var longitude : Double = 0.0;
 
-   fun HospitalListViewModel(var aName: String, var aLatitude : double, var aLongitude: double)
-   {
-       name = aName
-       latitude = aLatitude
-       longitude = aLongitude
-   }
-   */
+      fun HospitalListViewModel(aName: String, aLatitude : Double, aLongitude: Double)
+      {
+          name = aName
+          latitude = aLatitude
+          longitude = aLongitude
+      }
 
     private val _text = MutableLiveData<String>().apply {
-        value = "These are the hospitals: \"Providence Brigeport, Providence Mercantile, Providence St. Vincent\", Providence Newberg"
-        //value = latitude + longitude
+        //value = "These are the hospitals: \"Providence Brigeport, Providence Mercantile, Providence St. Vincent\", Providence Newberg"
+        value = name + " " + latitude + " " + longitude
     }
     val text: LiveData<String> = _text
 }
