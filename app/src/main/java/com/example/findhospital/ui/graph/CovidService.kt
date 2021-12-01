df3a644a9f1b277e19f5ee25c45ba621097940f5
aalshawa97 <abdullahalshawa@gmail.com>
2021-12-01 08:32:01 -0800

    Added COVID service

--- app/src/main/java/com/example/findhospital/ui/graph/CovidService.kt
+++ app/src/main/java/com/example/findhospital/ui/graph/CovidService.kt
@@ -1,5 +1,12 @@
 package com.example.findhospital.ui.graph
 
-class CovidService {
+import retrofit2.Call
+import retrofit2.http.GET
 
+interface CovidService {
+    @GET("us/daily.json")
+    fun getNationalData(): Call<List<CovidData>>
+
+    @GET("states/daily.json")
+    fun getStatesData(): Call<List<CovidData>>
 }
